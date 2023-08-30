8c80626616f95a31e0873d3725b23956a4b445b1
Alexandr Fomenko <sasha522000@gmail.com>
2023-08-30 12:32:13 +0300

    fix: Added support for runtiveConfig from envs

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,6 +3,13 @@ import MyModule from '../'
 
 export default defineNuxtConfig({
   modules: [MyModule],
+  runtimeConfig: {
+    public: {
+      yandexMetrika: {
+        id: '', // NUXT_PUBLIC_YANDEX_METRIKA_ID
+      },
+    },
+  },
   yandexMetrika: {
     id: '49439650',
   },
