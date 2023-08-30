5352cb699c82b813c8eeed7419c3746ad09c9176
Alexandr Fomenko <sasha522000@gmail.com>
2023-08-30 13:07:34 +0300

    fix: Remove unnecessary code from playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,13 +3,6 @@ import MyModule from '../'
 
 export default defineNuxtConfig({
   modules: [MyModule],
-  runtimeConfig: {
-    public: {
-      yandexMetrika: {
-        id: '', // NUXT_PUBLIC_YANDEX_METRIKA_ID
-      },
-    },
-  },
   yandexMetrika: {
     id: '49439650',
   },
